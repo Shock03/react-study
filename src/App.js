71cc9997f1426d882333f5780c7eb3e64134c438
Breno Marques <brenocarvalho10@gmail.com>
2024-02-29 13:26:29 -0300

    react hook, useState

--- src/App.js
+++ src/App.js
@@ -1,30 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 // Importação do componente Button e Header dentro do JSX
 import Button from './components/Button/button';
 import Header from './components/header/header';
 
 
 function App() {
-  // Evento criado
+
+    /*Primeiro item array (estado atual da pág/Hook), Segundo valor vai ser a função que vai alterar esse valor
+    | por padrão colocar o valor atual com o set na frente
+    --> HOOK (useState)*/
+    const [carregando, setCarregando] = useState(true)
+
+
+  
+  /* -->Evento criado
   function evento() {
     alert('O evento foi disparado!')
   }
+  -->Evento que não vai ser disparado por meio do JSX
+    window.addEventListener('scroll', () => console.log('Evento aconteceu'))
+  -->O primeiro parametro é o evento que quero disparar (scroll), 
+    o segundo é a função que vai ser disparada, o callback que vai ser disparado com a ação do scroll*/
 
-  //Evento que não vai ser disparado por meio do JSX
-  window.addEventListener('scroll', () => console.log('Evento aconteceu'))    // O primeiro parametro é o evento que quero disparar (scroll), o segundo é a função que vai ser disparada, o callback que vai ser disparado com a ação do scroll
 
-  //O que vai ser visualizado em tela é o que está dentro do return
+
+  /*-->O que vai ser visualizado em tela é o que está dentro do return*/
   return (
-    //React.Fragments --> JSX
-    <div style={{height: '200vh'}}>                                                      {/*style para a pág ter scroll*/}
-      <Header/>                                                                          {/*Chamando a função Header(header.js)*/}
-      <Button onClick={() => evento()} name='Botão 1' active={true}/>                    {/*Chamando a função Button(button.js) através de uma TAG, graças ao Babel*/}
-      <Button name='Botão 2' active={false}/>
-      <Button name='Botão 3' active=
-      {false}/>                                                                          {/*Passando o argumento, do componente Button, em formato de atributo*/}
-      <span onClick={() => evento()}>Mensagem qualquer</span>                            {/*Disparando o evento com o onClick*/}
+    /*React.Fragments --> JSX*/
+
+    <div>
+      {/*Chamando a função Button(button.js) através de uma TAG, graças ao Babel,
+      disparando o evento com o onClick | onClick={() => evento()}*/}
+      {carregando ? <span>Carregando...</span> : <Button name='Botão 1' active={true}/>}
+      <button onClick={() => setCarregando(false)}>Carregar site</button>
     </div>
   );
 }
